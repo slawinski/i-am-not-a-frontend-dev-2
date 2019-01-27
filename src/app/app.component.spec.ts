57e3c35cd5804d94b8e0deefc7769c1408d1ad85
piotr-s <piotr.slawinski1@gmail.com>
2019-01-27 17:52:22 +0100

    Make tests green

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'iamnotafed'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('iamnotafed');
+    expect(app.Title).toEqual('Frontend Dev');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render video in section tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to iamnotafed!');
+    expect(compiled.querySelector('section > video > source').textContent).toBeDefined('Welcome to iamnotafed!');
   });
 });

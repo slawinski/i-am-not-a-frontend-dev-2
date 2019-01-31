707a464618259fed3ab792a0b83105ffdc882925
piotr-s <piotr.slawinski1@gmail.com>
2019-01-31 17:12:57 +0100

    Implement backdrop toggle in component

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,17 +7,15 @@ import { FormControl } from '../../../node_modules/@angular/forms';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
-
   mode = new FormControl('over');
+  hasBackdrop = new FormControl('false');
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }

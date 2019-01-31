692f6c3486e272b6aef576e71c8c56a432735097
piotr-s <piotr.slawinski1@gmail.com>
2019-01-31 20:45:24 +0100

    Modify styling of the contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-  constructor() { }
-
-  ngOnInit() {
+  getErrorMessage() {
+    return this.email.hasError('required')
+      ? 'You must enter a value'
+      : this.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
   }
 
+  ngOnInit() {}
 }
